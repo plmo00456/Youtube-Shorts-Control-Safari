436eb379a5b90081e8dd2582971e0f29f04484b9
wj <park_wj7269@naver.com>
2024-01-17 15:03:48 +0900

    INIT

---
+++ Youtube-Shorts-Control-Safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Youtube-Shorts-Control-Safari
+//
+//  Created by 박우진 on 1/17/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
